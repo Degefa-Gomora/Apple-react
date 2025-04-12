f8a855a519efa4336df5df351cef9aacb68c3d2f
Degefa Gomora <degefag21@gmail.com>
2025-04-12 10:42:53 +0300

    app.css and index.css commented and assets removed

--- react-apple/src/App.jsx
+++ react-apple/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
 
-  return (
-    <>
+//   return (
+//     <>
       
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
-export default App
+// export default App
